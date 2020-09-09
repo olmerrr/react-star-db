fa7df815cf8f07030cee91f15ba8a98cf70aab90
Yaroslav <dombrovskiy91@gmail.com>
2020-09-09 13:04:04 +0300

    refactoring fetch req

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-fetch('https://swapi.dev/api/people/1/')
-    .then( (res) => {
-        return res.json();
-    })
-    .then((body) =>{
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const body = await res.json();
+    return body;
+};
+getResource('https://swapi.dev/api/people/1/')
+    .then((body) => {
         console.log(body);
-})
+    });
+
+
+// fetch('https://swapi.dev/api/people/1/')
+//     .then( (res) => {
+//         return res.json();
+//     })
+//     .then((body) =>{
+//         console.log(body);
+// })
