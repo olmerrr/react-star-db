7c653848b63fa47212ae61e0a62df50e77200cb5
Yaroslav <dombrovskiy91@gmail.com>
2020-09-09 19:07:24 +0300

    list names Star Wars

--- src/index.js
+++ src/index.js
@@ -6,44 +6,29 @@
 //         console.log(body) // создал еще промис и вывел данные
 //     });
 class SwapiService {
-    _apiBase = 'https://swapi.dev/api'; //"_"означает, что это приватная часть класса,
+
+    _apiBase = 'https://swapi.dev/api'; // "_"означает, что это приватная часть класса,
     // ее не следует использ или измен с наружи
     async getResource(url) {
         const res = await fetch(`${this._apiBase}${url}`); // await - означает ждать
 
-        //в этом блоке я  делаю ошибку от сервера
         if (!res.ok) {
             throw new Error(`Could not fetch ${url},`
                 + ` received ${res.status}`)
         }
-        //в этом блоке я  делаю ошибку от сервера
-
-        const body = await res.json();
-        return body;
+    return  await res.json();
     }
-    async getAllPeople() {
-        const res =  await this.getResource(`/people/`)
-        return res.results
+    async getAllPeople(){
+        const res = await this.getResource(`/people/`);
+        return res.results;
     }
-    getPerson(id) {
-        return this.getResource(`/people/${id}/`)
-    }
-    async getAllPlanets() {
-        const res =  await this.getResource(`/planets/`)
-        return res.results
-    }
-    getPlanet(id) {
-        return this.getResource(`/planets/${id}/`)
-    }
-    async getAllStarships() {
-        const res =  await this.getResource(`/starships/`)
-        return res.results
-    }
-    getStarship(id) {
-        return this.getResource(`/starships/${id}/`)
+    getAllPerson(id){
+        return this.getResource(`/people/${id}/`);
     }
 }
 const swapi = new SwapiService();
-swapi.getAllPerson(7).then( (p) => {
+swapi.getAllPeople().then((people) =>{
+    people.forEach( (p) =>{
         console.log(p.name)
+    })
 });
