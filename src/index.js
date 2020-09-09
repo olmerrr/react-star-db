50aeffd645f1b57b56aa99ad1c57cbd69c538586
Yaroslav <dombrovskiy91@gmail.com>
2020-09-09 15:12:44 +0300

    fetch + .catch  + errors from server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
+// fetch('https://swapi.dev/api/people/1/')
+//     .then((body) => {
+//     return body.json();
+//     }) //получил данные
+//     .then((body) => {
+//         console.log(body) // создал еще промис и вывел данные
+//     });
+
 const getResource = async (url) => {
-    const res = await fetch(url);
+    const res = await fetch(url); // await - означает ждать
+
+    //в этом блоке я  делаю ошибку от сервера
+    if (!res.ok){
+        throw new Error(`Could not fetch ${url},`
+        +` received ${res.status}`)
+    }
+    //в этом блоке я  делаю ошибку от сервера
+
     const body = await res.json();
     return body;
 };
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/12121ds/')
     .then((body) => {
         console.log(body);
+    })
+    .catch((err) => {
+        console.error('Problems baby', err)
     });
-
-
-// fetch('https://swapi.dev/api/people/1/')
-//     .then( (res) => {
-//         return res.json();
-//     })
-//     .then((body) =>{
-//         console.log(body);
-// })
+//.catch - ошибка которую пишем мы
