1b8343a2ca51c9637bba64e863b7d7a193186106
Yaroslav <dombrovskiy91@gmail.com>
2020-09-10 16:12:02 +0300

    refactoring swapi-services + make component RandomPlanet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,9 +1,73 @@
 import React, {Component} from "react";
 import './random-planet.css';
+import SwapiService from "../../services/swapi-services";
 
 export default class RandomPlanet extends Component {
-    render(){
-        return <p>random planet</p>
+    swapiService = new SwapiService()
+    state = {
+        id: null,
+        name: null,
+        population: null,
+        rotationPeriod: null,
+        diameter: null
+    }
+    constructor() {
+        super();
+        this.updatePlanet();
+    }
+    updatePlanet() {
+        const id = Math.floor(Math.random() * 25 + 2);
+        this.swapiService
+            .getPlanet(id)
+            .then((planet) => {
+                this.setState({
+                    id,
+                    name: planet.name,
+                    population: planet.population,
+                    rotationPeriod: planet.rotation_period,
+                    diameter: planet.diameter
+                })
+            });
+    }
+
+    render() {
+        const {id,name, population, rotationPerion, diameter} = this.state;
+        return (
+
+            <div className="random-planet jumbotron rounded">
+                <img
+                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                    alt="planet" className="planet-image"/>
+
+                <h4>{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">
+                            Population
+                        </span>
+                        <span>
+                            {population}
+                        </span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">
+                            Rotation Period
+                        </span>
+                        <span>
+                            {rotationPerion}
+                        </span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">
+                            Diameter
+                        </span>
+                        <span>
+                            {diameter}
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        )
 
     }
 }
--- src/services/swapi-services.js
+++ src/services/swapi-services.js
@@ -1,33 +1,83 @@
+export default class SwapiService {
 
-// fetch('https://swapi.dev/api/people/1/')
-//     .then((body) => {
-//     return body.json();
-//     }) //получил данные
-//     .then((body) => {
-//         console.log(body) // создал еще промис и вывел данные
-//     });
-export  default class SwapiService {
+    _apiBase = 'https://swapi.dev/api';
 
-    _apiBase = 'https://swapi.dev/api'; // "_"означает, что это приватная часть класса,
-    // ее не следует использ или измен с наружи
     async getResource(url) {
-        const res = await fetch(`${this._apiBase}${url}`); // await - означает ждать
+        const res = await fetch(`${this._apiBase}${url}`);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch ${url},`
-                + ` received ${res.status}`)
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`)
         }
-        return  await res.json();
+        return await res.json();
     }
-    async getAllPeople(){
+
+    async getAllPeople() {
         const res = await this.getResource(`/people/`);
-        return res.results;
+        return res.results.map(this._transformPerson);
     }
-    getPerson(id){
-        return this.getResource(`/people/${id}/`);
+
+    async getPerson(id) {
+        const person = await this.getResource(`/people/${id}/`);
+        return this._transformPerson(person);
     }
-};
-const swapi = new SwapiService();
-swapi.getAllPerson(3).then((p) =>{
-    console.log(p.name)
-});
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results.map(this._transformPlanet);
+    }
+
+    async getPlanet(id) {
+        const planet = await this.getResource(`/planets/${id}/`);
+        return this._transformPlanet(planet);
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results.map(this._transformStarship);
+    }
+
+    async getStarship(id) {
+        const starship = this.getResource(`/starships/${id}/`);
+        return this._transformStarship(starship);
+    }
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/;
+        return item.url.match(idRegExp)[1];
+    }
+
+    _transformPlanet(planet) {
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotation_period,
+            diameter: planet.diameter
+        };
+    }
+
+    _transformStarship(starship) {
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
+
+    _transformPerson(person) {
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
+    }
+}
