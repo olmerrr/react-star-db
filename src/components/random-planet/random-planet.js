11e180508c14baae8d9e98fa019ed5d779b9f7ba
Yaroslav <dombrovskiy91@gmail.com>
2020-09-11 14:34:13 +0300

    <Spinner/> preloader component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import './random-planet.css';
 import SwapiService from "../../services/swapi-services";
+import Spinner from "../spinner";
 
 export default class RandomPlanet extends Component {
 
@@ -27,38 +28,40 @@ export default class RandomPlanet extends Component {
         const { planet:{id, name, population, rotationPeriod, diameter} } = this.state;
         return (
 
-            <div className="random-planet jumbotron rounded">
-                <img
-                    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                    alt="planet" className="planet-image"/>
 
-                <h4>{name}</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <span className="term">
-                            Population
-                        </span>
-                        <span>
-                            {population}
-                        </span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">
-                            Rotation Period
-                        </span>
-                        <span>
-                            {rotationPeriod}
-                        </span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">
-                            Diameter
-                        </span>
-                        <span>
-                            {diameter}
-                        </span>
-                    </li>
-                </ul>
+            <div className="random-planet jumbotron rounded">
+              <Spinner/>
+                {/*<img*/}
+                {/*    src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}*/}
+                {/*    alt="planet" className="planet-image"/>*/}
+
+                {/*<h4>{name}</h4>*/}
+                {/*<ul className="list-group list-group-flush">*/}
+                {/*    <li className="list-group-item">*/}
+                {/*        <span className="term">*/}
+                {/*            Population*/}
+                {/*        </span>*/}
+                {/*        <span>*/}
+                {/*            {population}*/}
+                {/*        </span>*/}
+                {/*    </li>*/}
+                {/*    <li className="list-group-item">*/}
+                {/*        <span className="term">*/}
+                {/*            Rotation Period*/}
+                {/*        </span>*/}
+                {/*        <span>*/}
+                {/*            {rotationPeriod}*/}
+                {/*        </span>*/}
+                {/*    </li>*/}
+                {/*    <li className="list-group-item">*/}
+                {/*        <span className="term">*/}
+                {/*            Diameter*/}
+                {/*        </span>*/}
+                {/*        <span>*/}
+                {/*            {diameter}*/}
+                {/*        </span>*/}
+                {/*    </li>*/}
+                {/*</ul>*/}
             </div>
         )
     }
