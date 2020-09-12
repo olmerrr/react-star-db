b15356e809def2220a51c0780e02210ce373469c
Yaroslav <dombrovskiy91@gmail.com>
2020-09-12 17:57:36 +0300

    component ErrorIndicator its my custom error-component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,53 +2,59 @@ import React, {Component} from "react";
 import './random-planet.css';
 import SwapiService from "../../services/swapi-services";
 import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
 
 export default class RandomPlanet extends Component {
 
     swapiService = new SwapiService();
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     };
 
     constructor() {
         super();
         this.updatePlanet();
     }
-
+    onError = (err) =>{
+        this.setState({
+            error: true,
+            loading: false
+        });
+    }
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
             loading: false
         })
-
     };
 
     updatePlanet() {
-        const id = Math.floor(Math.random() * 25 + 2);
+        const id = Math.floor(Math.random() * 1125 + 2);
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
+            .catch(this.onError)
     }
-
     render() {
-        const { planet, loading } = this.state;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !loading ? <PlanetViev planet={planet}/> : null;
+        const {planet, loading, error} = this.state;
 
-        // if (loading) {
-        //     return <Spinner/>
-        // };
+       const hasData = !(loading || error);
+        const errorMessage = error ? <ErrorIndicator/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = hasData ? <PlanetView planet={planet}/> : null;
+
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
-
             </div>
         )
     }
 }
-const PlanetViev = ({planet}) => {
+const PlanetView = ({planet}) => {
     const {id, name, population, rotationPeriod, diameter} = planet;
     return (
         <React.Fragment>
