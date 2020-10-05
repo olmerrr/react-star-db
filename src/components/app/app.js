252b79c31d0b93084c91d571afde8bc94cb4f7bf
Yaroslav <dombrovskiy91@gmail.com>
2020-10-05 10:23:12 +0300

    Dynamic path for Route

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -12,6 +12,8 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 import './app.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import StarshipDetails from "../sw-components/starship-details";
+import {StarshipList} from "../sw-components";
 
 export default class App extends Component {
 
@@ -43,7 +45,13 @@ export default class App extends Component {
                     exact={true}/>
               <Route path="/people" component={PeoplePage} />
               <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" component={StarshipsPage} />
+              <Route path="/starships" exact component={StarshipsPage} />
+              <Route path="/starships/:id"
+                  render = {({match}) => {
+                    // console.log(match)
+                    const {id} = match.params;
+                    return <StarshipDetails itemId = {id} />
+                  }}/>
 
             </div>
           </Router>
